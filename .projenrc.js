34b7dc76a6ca24718340d23d944490f512a8a086
Neil Kuan <guan840912@gmail.com>
2022-05-07 16:04:07 +0800

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,7 @@
+const { awscdk } = require("projen");
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "stepn-updater",
+});
+project.synth();
